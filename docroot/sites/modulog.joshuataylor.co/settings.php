c83a91d9025fa7bcc1a94e39c013938312d2e559
Joshua Taylor <joshua.taylor@acquia.com>
2013-03-31 16:25:26 -0500

    correcting a path in fast 404 settings

--- docroot/sites/modulog.joshuataylor.co/settings.php
+++ docroot/sites/modulog.joshuataylor.co/settings.php
@@ -537,7 +537,7 @@ $conf['memcache_key_prefix'] = 'modulog';
 
 # Load the fast_404.inc file. This is needed if you wish to do extension
 # checking in settings.php.
-include_once('./sites/all/modules/fast_404/fast_404.inc');
+include_once('./sites/all/modules/contrib/fast_404/fast_404.inc');
 
 # Disallowed extensions. Any extension in here will not be served by Drupal and
 # will get a fast 404.
